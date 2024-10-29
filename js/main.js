6cb1b19113cce53e6124aa3ccf93db2297ab1d7d
Annika Karlsson <en_anka@hotmail.com>
2024-10-29 19:05:02 +0000

    Adding starting layout

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/**
+ *  OMDb template
+ *	Documentation: http://www.omdbapi.com/
+ *  Generate an API key here: http://www.omdbapi.com/apikey.aspx
+ */
+
+
+/**
+* According to documentation, you need at least 2 parameters when calling the API http://www.omdbapi.com/
+* 1 Required parameter: apikey
+* 2 Required parameter: One of the following i=, t= or s=
+*
+* 
+* Example with parameter s=star trek
+* http://www.omdbapi.com/?apikey=[yourkey]&s=star trek
+*
+* Example with parameter s=star trek AND y=2020
+* http://www.omdbapi.com/?apikey=[yourkey]&s=star trek&y=2020
+*
+* Example with parameter s=star trek AND type=series
+* http://www.omdbapi.com/?apikey=[yourkey]&s=star trek&type=series
+*
+*/
+//let url = 'http://www.omdbapi.com/?apikey=[yourkey]=star trek';
+
+
+/* 
+https://developer.edamam.com/edamam-docs-recipe-api
+id=3084edd9
+
+key=7114784343669c87f0effc510b0e4879 */
+
+showRecipies(){
+    //FetchData() Hämtar data och ger tillbaks resultatetet som ett promise-object om inga felmeddelanden
+}
+
