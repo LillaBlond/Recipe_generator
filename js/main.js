f6a9741978c6fbf2eb254446b8fc19d55c5c59a6
Annika Karlsson <en_anka@hotmail.com>
2024-10-31 09:59:11 +0000

    Functional site with fetching API data and creating elements to the website

--- js/main.js
+++ js/main.js
@@ -23,14 +23,79 @@
 */
 //let url = 'http://www.omdbapi.com/?apikey=[yourkey]=star trek';
 
+//	&#9206; up arrow
+ 
+//https://developer.edamam.com/edamam-docs-recipe-api
+const id = "app_id=3084edd9";
+const key="app_key=7114784343669c87f0effc510b0e4879";
 
-/* 
-https://developer.edamam.com/edamam-docs-recipe-api
-id=3084edd9
-
-key=7114784343669c87f0effc510b0e4879 */
-
-showRecipies(){
-    //FetchData() Hämtar data och ger tillbaks resultatetet som ett promise-object om inga felmeddelanden
+async function showRecipies(){
+    const addedFilters = checkFilterSelection();
+    const search = "&q=" + document.getElementById("searchField").value;
+    let link = `https://api.edamam.com/api/recipes/v2?type=any&beta=false&${id}&${key}&${search}${addedFilters}`
+  
+    try{
+        const response = await fetch(link);
+        if(!response.ok) {
+            throw new Error (`HTTP error code: ${response.status}, HTTP error message: ${response.statusText}`);
+        }
+        const data = await  response.json();
+        console.log(link);
+        console.log(data);
+        addRecipeCard(data);
+    }catch(error){
+        console.log(error)
+    }
 }
 
+function addRecipeCard(api_data){
+
+    const hits = api_data.hits;
+    
+    const recipeCard = hits.map((element) => {
+
+        return recipeData = `<article class="recipeBox">
+            <img src="${element.recipe.image}" alt="Här ska det vara en bild">
+            <section class="recipeSlideText">
+                <h2>${element.recipe.label}</h2>
+                <p class="smallInfo divider"><span>${Math.round(element.recipe.calories)}</span> Calories</p>
+                <p class="smallInfo divider"><span>${element.recipe.ingredients.length}</span> Ingredients</p>
+                <p class="smallInfo">${showCookingTime(element.recipe.totalTime)}</p>
+            </section>
+        </article>`
+    }).join("");
+
+    document.getElementById("recipesContainer").innerHTML = recipeCard;
+}
+
+function checkFilterSelection(){
+    const dietValuesArr = document.querySelectorAll(".filterSection input[name=diet]");
+    const allergiesFilterArr = document.querySelectorAll(".filterSection input[name=health]");
+    const mealTypeArr = document.querySelectorAll(".filterSection input[name=mealType]");
+    let filterValues = "";
+
+    dietValuesArr.forEach((selection) => {
+        if (selection.checked === true) filterValues += "&diet=" + selection.value;
+    });
+
+    allergiesFilterArr.forEach((selection) => {
+        if (selection.checked === true) filterValues += "&health=" + selection.value;
+    });
+    
+    mealTypeArr.forEach((selection) => {
+        if (selection.checked === true) filterValues += "&mealType=" + selection.value;
+    });
+
+    return filterValues;
+}
+
+
+function showCookingTime(time){
+    const minutes = time % 60;
+    const hours = Math.floor(time/60);
+
+    if(hours && minutes) return `${hours} h and ${minutes} min`;
+    else if (hours && !minutes) return `${hours} h`;
+    else if (!hours && minutes) return `${minutes} min`;
+    else return "n/a";
+}
